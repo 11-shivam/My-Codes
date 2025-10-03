55ab44d7d7b05f9890ece83460d3e40f9ae3c20a
11-shivam <shivam74720@gmail.com>
2025-10-04 01:13:44 +0530

    Binary Search algorithm Iterative

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -2,7 +2,28 @@
 #include <vector>
 using namespace std;
 
+int binarySearch(vector<int>nums , int n , int target) {
+  int st = 0 , end = n-1;
+
+  while ( st <= end ) {
+    int mid = st + (end-st)/2; // optimized to ovecome overflow scenerio
+    if (nums[mid] == target ) {
+      return mid;
+    } else if (target < nums[mid]) { //move to 1st half of vector
+      end = mid - 1;
+    } else {
+      st = mid + 1;
+    }
+  }
+  return -1;
+}
+
 int main () {
-  
+  vector<int> nums = {5,10,15,20};
+  int n = nums.size();
+  int key = 5; // output = index of 5 -> 0
+
+  cout<<binarySearch(nums , n , key) <<endl;
+
   return 0;
 }
